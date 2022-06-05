526eba881b35812e640720bcaa43bcd14c0ca983
HYCH <choehyeonjin74@gmail.com>
2022-06-05 21:03:07 +0900

    코드 최신화

--- study_system(frst).py
+++ study_system(frst).py
@@ -1,12 +1,12 @@
-Seat_list = list(range(1,1+1))   #좌석수 1개로 가정__실제로는 더 많으나 일단 1인칭시점에서 적용된다고 가정함
-student_Dict={}                   #학생 리스트
-while Seat_list:
-    n1=int(input('좌석번호를 입력하세요: '))
-    if n1>1000 or n1<0:
+seat_list=list(range(1,100+1))
+student_dict={}
+def admin():
+    n1 = int(input('좌석번호를 입력하세요: '))
+    if n1 not in seat_list:
         print('이용가능한 좌석이 아닙니다. 다시 입력해주세요.')
-        continue
     n2 = input('이름을 입력하세요: ')
-    n3 = int(input('이용하실 시간을 입력하세요(단위: 초): '))
-    student_Dict[n2]=n3
-    break
-#수정 필요: 
+    n3 = int(input('이용하실 시간을 입력하세요: '))
+    student_dict[n2] = n3
+    seat_list.remove(n1)
+while seat_list:
+    admin()
