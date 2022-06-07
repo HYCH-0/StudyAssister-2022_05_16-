38507cdaf31c82bb31123aa12e63fde04d6b9625
HYCH <choehyeonjin74@gmail.com>
2022-06-07 12:12:51 +0900

    오류코드 임의 주석처리

--- student_information.py
+++ student_information.py
@@ -1,12 +1,22 @@
 seat_list=list(range(1,100+1))
 student_dict={}
 n1, n2, n3 = 0, 0, 0
-def admin(student_dict, seat_list, n1, n2, n3):
+
+def bool_seat(student_dict, n1):
     n1 = int(input('좌석번호를 입력하세요: '))
     if n1 not in seat_list:
         print('이용가능한 좌석이 아닙니다. 다시 입력해주세요.')
+        bool_seat(student_dict, n1)
+    #seat_list.remove(n1)
+
+def admin(student_dict, seat_list, n1, n2, n3):
+    bool_seat(student_dict, n1)
+    
     n2 = input('이름을 입력하세요: ')
     n3 = int(input('이용하실 시간을 입력하세요: '))
     student_dict[n2] = n3
-    seat_list.remove(n1)
+    
+    print(n1, n2, n3)
     return n1, n2, n3
+
+admin(student_dict, seat_list, n1, n2, n3)
